c646a60d3bc2bd8ac76e2d36181d90af64e2cb58
julioreisdev <juliosteam0352@gmail.com>
2022-10-16 09:17:17 -0300

    Adiciona data em rota /test

--- src/routes/testRoutes.ts
+++ src/routes/testRoutes.ts
@@ -4,7 +4,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/test", (req: Request, res: Response) => {
-  return res.status(200).send("OK");
+  return res.status(200).send("OK - 16/10");
 });
 
 export default router;
